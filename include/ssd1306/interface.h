09799fcfed07a1bd2dbb40ecf64b82895fcd33f5
Dann2405 <danielsoares0558@gmail.com>
2025-02-20 09:59:16 -0300

    feat: configuração padrão para o display OLED

---
+++ include/ssd1306/interface.h
@@ -0,0 +1,8 @@
+#ifndef interface_h
+#define interface_h
+
+// protótipos das funções
+void displayssd1306_init(); // Inicializa o display SSD1306
+static inline void update_display(); // Atualiza o display
+
+#endif // interface_h
