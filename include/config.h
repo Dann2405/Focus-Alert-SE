3c7d632c3fd484fc59247a012e9959e28027901f
Dann2405 <danielsoares0558@gmail.com>
2025-02-20 10:10:48 -0300

    feat: config.h adicionado para configurações e definições do projeto

---
+++ include/config.h
@@ -0,0 +1,28 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Configurações e definições do projeto (pinos, tempos, limites, etc.)
+
+// definição dos pinos do LED RGB
+const uint LED_RED = 13;
+const uint LED_GREEN = 11;
+const uint LED_BLUE = 12;
+
+// definição dos pinos dos botões
+const uint BUTTON_A = 5;
+const uint BUTTON_B = 6;
+
+// definição dos pinos dos buzzer
+const uint Buzzer_A = 21;
+const uint Buzzer_B = 10;
+
+// definição do pino da matrix 5x5
+const uint MATRIX_LED = 7;
+
+// definição dos pinos do joystick
+const uint VRX_PIN = 27;
+const uint VRY_PIN = 26;
+const uint SW_PIN = 22;
+
+
+#endif // CONFIG_H
