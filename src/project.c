14c1dad4ce1f5cba12ca10c6681ae66a70bf5677
Dann2405 <danielsoares0558@gmail.com>
2025-02-20 10:01:33 -0300

    feat: interface.c implementado para testar a utilização do display OLED

--- src/project.c
+++ src/project.c
@@ -1,22 +1,16 @@
-#include <stdio.h>
-#include "pico/stdlib.h"
-#include "hardware/spi.h"
-#include "hardware/i2c.h"
-#include "hardware/dma.h"
-#include "hardware/pio.h"
-#include "hardware/interp.h"
-#include "hardware/timer.h"
-#include "hardware/watchdog.h"
-#include "hardware/clocks.h"
-#include "hardware/uart.h"
-
+#include "include/lib/bibliotecas.h"
+#include "interface.c"
 
 int main()
 {
     stdio_init_all();
+
+    displayssd1306_init();
+    
     
     while (true) {
         printf("Hello, world!\n");
+        update_display();
         sleep_ms(1000);
     }
 }
