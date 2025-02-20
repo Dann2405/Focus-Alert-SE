d8e50ff86a67a99ebdd408cf0dff79749469695f
Dann2405 <danielsoares0558@gmail.com>
2025-02-20 09:57:56 -0300

    feat: bibliotecas padrão da linguagem C e do SDK Raspberry Pi

---
+++ include/lib/bibliotecas.h
@@ -0,0 +1,18 @@
+#ifndef BIBLIOTECAS_H
+#define BIBLIOTECAS_H
+
+// bibliotecas padrão do SDK
+#include <stdio.h>
+#include <stdlib.h>
+#include "pico/stdlib.h"
+#include "hardware/spi.h"
+#include "hardware/i2c.h"
+#include "hardware/dma.h"
+#include "hardware/pio.h"
+#include "hardware/interp.h"
+#include "hardware/timer.h"
+#include "hardware/watchdog.h"
+#include "hardware/clocks.h"
+#include "hardware/uart.h"
+
+#endif // BIBLIOTECAS_H
